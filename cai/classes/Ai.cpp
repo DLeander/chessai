6f13ddece7a80001ba7532eb81b17f3efbb28b1c
root <root@Dexter.localdomain>
2023-10-10 16:26:22 +0200

    Added files for the implementation of the AI.

---
+++ cai/classes/Ai.cpp
@@ -0,0 +1,14 @@
+#include "../headers/Ai.h"
+#include "../headers/Board.h"
+#include "../headers/Piece.h"
+#include <iostream>
+#include <vector>
+
+Ai::Ai() {
+    // TODO
+
+}
+
+Ai::~Ai(){
+    // TODO
+}
---
+++ cai/headers/Ai.h
@@ -0,0 +1,25 @@
+#pragma once  // Include guards to prevent multiple inclusion
+
+// #include "Move.h"
+#include "Board.h"
+
+// Forward declaration
+class Board;
+
+class Ai {
+public:
+    Ai();  // Constructor
+    ~Ai(); // Destructor
+
+    // Member functions
+
+
+    // Getters
+
+
+    // Setters
+
+
+private:
+
+};
